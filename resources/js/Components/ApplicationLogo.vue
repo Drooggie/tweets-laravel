ec6659ada6d53f2d281275d1f61a6822a7a4fd36
Oraz <oraznorlan2@gmail.com>
2024-11-01 23:43:49 +0500

    init/ small functionality

---
+++ resources/js/Components/ApplicationLogo.vue
@@ -0,0 +1,3 @@
+<template>
+    <img src="/images/crow-logo.jpg" class="rounded-full" alt="" style="height: 40px; width: 40px;">
+</template>
---
+++ resources/js/Pages/Tweets/Index.vue
@@ -0,0 +1,30 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import InputError from '@/Components/InputError.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import { useForm, Head } from '@inertiajs/vue3';
+
+const form = useForm({
+    message: '',
+})
+</script>
+
+<template>
+    <Head title="Tweets" />
+
+    <AuthenticatedLayout>
+        <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+            <form @submit.prevent="form.post(route('tweets.store'), {onSuccess: () => form.reset()})">
+                <textarea 
+                    v-model="form.message"
+                    placeholder="Place to tweet"
+                    class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm bg-black text-white placeholder-white"
+                    >
+                </textarea>
+                <InputError :message="form.errors.message" class="mt-2"/>
+                <PrimaryButton class="mt-4">Tweet</PrimaryButton>
+            </form>
+        </div>
+    </AuthenticatedLayout>
+
+</template>
